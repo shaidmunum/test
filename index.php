9d493563b69097b1ae1556c938a5097764e0a75b
Shahid Basheer <shahid.basheer@ebryx.com>
2018-02-14 12:00:28 +0500

    Initial changes

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+
+<html>
+<head>
+    <title>TITLE </title>
+    <link rel="stylesheet" type="text/css" href ="style.css" >
+</head>
+
+<body>
+     <div class="main_wrapar" >
+                 
+   </div>  
+
+          
+          
+           <div  class="header_wraper">
+           <img src="1.jpeg" >
+             
+          </div>
+          
+          
+          
+          
+      <div id="navbar">
+        <ul id="menu">
+            <li> <a href="#" >HOME</a></li>
+            <li> <a href="#" >ALL PRODUCTS</a></li>
+            <li> <a href="#" >MY ACCOUNT</a></li>
+            <li> <a href="#" >SIGN UP</a></li>
+            <li> <a href="#" >SHOPPING CART</a></li>
+            <li> <a href="#" >CONTACT US</a></li>
+            
+        </ul>  
+     </div>
+      <div class="content_wraper" >
+        <div class="leftcontent_wraper" >
+        left content
+        </div>
+         
+             <div class="rightcontent_wraper" >
+        right content
+             </div>
+    
+           
+    </div>
+            <div class="footer" >
+        footer
+             </div>
+            
+</body>
+</html>
