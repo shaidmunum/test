07ff50f1589aeea5c2bd7380889a0ea6996dc529
Shahid Basheer <shahid.basheer@ebryx.com>
2018-02-14 17:40:18 +0500

    sidebar changes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-    <title>TITLE </title>
+    <title>ONLINE STORE </title>
     <link rel="stylesheet" type="text/css" href ="style.css" >
 </head>
 
@@ -34,11 +34,31 @@
      </div>
       <div class="content_wraper" >
         <div class="leftcontent_wraper" >
-        left content
+        <div>
+             <div id="search">Search</div>
+             <form method="get" action="results.php<">
+                  <input type="text" name="user_query "  placeholder="Search A Product"/> 
+                  <input type="submit" name="search" value="search" />
+                     </form>
+         
+                  </div>
+        <div id="catagory">Category</div>
+        <ul id="cats">
+         <li><a href="#"> LAPTOPS</a></li>
+         <li><a href="#"> DESKTOPS</a></li>
+         <li><a href="#"> MOBILES</a></li>
+         <li><a href="#"> CAMERA</a></li>
+         <li><a href="#"> APPLIENCES</a></li>
+         <li><a href="#"> TABLE</a></li>
+         <li><a href="#"> HEAD PHONES</a></li>
+
+        </ul>
+   
         </div>
          
              <div class="rightcontent_wraper" >
-       <H1>WE MAKE YOU FEEL EASIER IN SELETING THINGS...</H1>
+       <H4>WE MAKE YOU FEEL EASIER IN SELETING THINGS...</H4>
+                  
 
              </div>
     
